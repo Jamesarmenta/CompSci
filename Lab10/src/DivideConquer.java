9bb2bd55667b4993993c1e6511c4cf2de758ec46
James Armenta <James.m.armenta@gmail.com>
2015-04-29 23:25:28 -0400

    Apr29

--- Lab10/src/DivideConquer.java
+++ Lab10/src/DivideConquer.java
@@ -1,9 +1,16 @@
 public class DivideConquer {
 	public static void main(String[] args) 
 	{
-		int myNums[] = {1, 2, 3, 4, 5, 6, 7, 7 ,7};
-
+		int myNums[] = {1, 3, 5, 7, 11, 1111, 8};
+		System.out.println("Original:");
 		printList(myNums);
+		System.out.println("Odds Before Evens:");
+		divide(myNums);
+	}
+
+	public static void divide(int list[])
+	{
+		divide(list, 0, list.length-1);
 	}
 
 	// sIdx is the index of the first element of the sublist
@@ -16,25 +23,13 @@ public class DivideConquer {
 			return;
 		}
 
-		// recursive step(s)
-		// which include dividing and conquering
-
-		// we know sublist length is >= 2
 		int midIdx = (sIdx + eIdx) / 2; // we know that it is integer div.
-		mergeSort(list, sIdx, midIdx);
-		mergeSort(list, midIdx+1, eIdx);
+		divide(list, sIdx, midIdx);
+		divide(list, midIdx+1, eIdx);
 
-		// when we get here, what do we know2 sublists that are sorted
-		// amongst themselves
-		// Left sublist goes from sIdx to midIdx 
-		// Right sublist goes from midIdx+1 to eIdx		
 		conquer(list, sIdx, midIdx, eIdx);
 	}
 
-	// We can assume that list has a sublist from sIdx to midIdx that is sorted
-	// and another sublist from midIdx+1 to eIdx that is also sorted
-	//
-	// result the sublist from sIdx to eIdx will be sorted
 	public static void conquer(int list[], int sIdx, int midIdx, int eIdx)
 	{
 		int tempList[] = new int[eIdx - sIdx + 1];
@@ -45,7 +40,7 @@ public class DivideConquer {
 		int i=0;
 		while (leftIdx <= midIdx && rightIdx <= eIdx)
 		{
-			if (list[leftIdx] >= list[rightIdx])
+			if (list[leftIdx] %2 == 0)
 			{
 				tempList[i] = list[rightIdx];
 				rightIdx++;
@@ -58,7 +53,9 @@ public class DivideConquer {
 			i++;
 		}
 
+
 		// only one of these two while loop will iterate >0 times
+
 		// copy all the elements still on the left into the temp
 		while (leftIdx <= midIdx)
 		{
@@ -82,13 +79,21 @@ public class DivideConquer {
 		{
 			list[listIdx] = tempList[j];
 		}
+		
+		//print on last go-around
+		if(i == list.length)
+		{
+			printList(list);
+		}
 	}
+
 
 	public static void printList(int list[])
 	{
 		for (int i=0; i< list.length; i++)
 		{
-			System.out.println(list[i]);
+			System.out.print(list[i] + " ");
 		}
+		System.out.println();
 	}
 }
